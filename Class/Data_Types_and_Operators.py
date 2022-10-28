604296af39af4162e9a486bc3e337b58cfe5702f
briviaallan <briviallan@gmail.com>
2022-10-28 20:37:38 +0300

    first commit

---
+++ Class/Data_Types_and_Operators.py
@@ -0,0 +1,98 @@
+from tkinter import Y
+
+
+print('hallo world!')
+
+# This is my path to becoming a python guru and beating the odds
+
+#################################################################
+
+#Data Types & Operators#
+
+#numerical data types#
+#intagers
+100000
+6
+3
+8
+9
+
+#float are decimal places
+1. # 1.0
+5000.0
+3.1428
+
+# complex values
+1 - 3j
+4g = 5
+
+###################################################################
+#boolean values
+True
+False
+#or even expressions with true or false options like :
+2 > 5 #is an eqivalent of false
+
+##################################################################
+#strings > THey are declared in single or double quotes.
+"Hallo WOrld!"
+'Hallo World!'
+'1' #or any intager in quotes is not a number, but a character representation.
+
+#########################################################################
+#collections > They are just a collection of values in a specific order.
+#List
+[1, 2, 3,'a']
+
+#Tuple > Like lista but immutable > THey're in Parenthesis
+(1, 2, 3, 'a')
+
+#set > Unordered values that don't allow duplicates.
+{1, 2, 3,}
+{1, 2, 2, 2, 'a'} # this is the same as 
+{1, 2, 'a'}
+
+#Dictionary > Collection of unordered key value pairs.
+{1: 'a', 2: 'b', 3: 'c'}
+
+##################################################################################
+################################These are your basic data types##################################
+
+#####################################################################################################
+
+                                 #Operators#
+
+#numerical operators
+1 + 2 #addition > Can be used on strings and lists > It concantinates the string.
+'a' + 'b' = 'ab'
+[1, 2] + ['a', 'b'] = [1, 2, 'a', 'b']
+
+10 - 5 #Subtraction
+(1 + 3j) **5 #Power ( **5 )
+
+2 * 2 = 4 # Multiplication
+
+1 / 2 = 0.5 # Division
+
+###################################
+#Asignment Operators
+x = 3 # The variable 'x' When called for will represent '3'
+
+#Comparison Operators
+1 < 5 # less than
+x > Y #greater than
+1 <= 5 #Less than or equal to
+'a' != 'abc' # NOt equal to
+1 == 1. # # Equal to
+
+# Logic OPerators
+# And, Or, Not
+(1 == 1) and (10 > 12) # for an and, both part of the statements need to be concurent to display a 'true' answer.
+1 > 2 or 3 > 2 # this gives a True value since only one needs to be true when using "or"
+not ('a' != 'b') #Will give a false Value since 'a' is not equal to be. "not" gives a negative feedback to a positive call
+
+#Membership Operators
+#This operator used to give values to other values in a list, Tuple, Set
+1 in [1, 2, 3,] = True
+1 not in [1, 2, 3] - False
+'a' in [1, 2, 3] - False
